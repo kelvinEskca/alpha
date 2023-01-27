717d60d8c48d71970c23838e92c7b63506e2c55d
kelvinEskca <beredugokelvin@gmail.com>
2023-01-27 17:30:43 +0100

    link fixed

--- src/Components/ImageCard.js
+++ src/Components/ImageCard.js
@@ -80,54 +80,54 @@ const ImageCard = ({toggleState}) => {
         products.length > 0 ? (
             products.map((item,i)=>{
                 return (
-                    <Link to={`details/${item._id}`}>
-                        <div className="box" key={i}>
-                            <div className="tag"><small></small></div>
-                            <div className="image-box">
+                    <div className="box" key={i}>
+                        <div className="tag"><small></small></div>
+                        <div className="image-box">
+                            <Link to={`details/${item._id}`}>
                                 <img src={`${item.image[0].url}`} alt={item.image[0].url} className="imageOne" />
                                 <img src={`${item.image[1].url}`} alt={item.image[1].url} className="imageTwo" />
-        
-                                <div className="quick-add">
-                                    <div className="quick-add-top">
-                                        <h3 className="heading">Quick Add +</h3>
-                                    </div>
-        
-                                    <div className="quick-add-bottom">
-                                        {item.sizes.map((size,i)=>{
-                                            return (<div className="size" key={i}><small className={`${size === selectedSize ? 'sizeActive' : ''}`} onClick={()=>handleClick(size,item)}>{size}</small></div>)
-                                        })}
-                                    </div>
+                            </Link>
+    
+                            <div className="quick-add">
+                                <div className="quick-add-top">
+                                    <h3 className="heading">Quick Add +</h3>
+                                </div>
+    
+                                <div className="quick-add-bottom">
+                                    {item.sizes.map((size,i)=>{
+                                        return (<div className="size" key={i}><small className={`${size === selectedSize ? 'sizeActive' : ''}`} onClick={()=>handleClick(size,item)}>{size}</small></div>)
+                                    })}
                                 </div>
                             </div>
-                            <div className="text">
-                                <h3 className="heading">{item.name}</h3>
-                                <p className="paragraph">{item.color}</p>
-                                <p className="paragraph">${item.price}</p>
-                            </div>
-        
-                            <div className="size-box">
-                                {item.sizes.map((size,i)=>{
-                                    return (<div className="size" key={i}><small>{size}</small></div>)
-                                })}
-                            </div>
-        
-                            <div className="images-box">
-                                <img src={`${item.image[0].url}`} alt={item.image[0]} className="imageOne" />
-                                <img src={`${item.image[0].url}`} alt={item.image[0]} className="imageOne" />
-                                <img src={`${item.image[0].url}`} alt={item.image[0]} className="imageOne" />
-                                <img src={`${item.image[0].url}`} alt={item.image[0]} className="imageOne" />
-                                <img src={`${item.image[0].url}`} alt={item.image[0]} className="imageOne" />
-                                <img src={`${item.image[0].url}`} alt={item.image[0]} className="imageOne" />
-        
-                                <img src={`${item.image[0].url}`} alt={item.image[0]} className="imageOne" />
-                                <img src={`${item.image[0].url}`} alt={item.image[0]} className="imageOne" />
-                                <img src={`${item.image[0].url}`} alt={item.image[0]} className="imageOne" />
-                                <img src={`${item.image[0].url}`} alt={item.image[0]} className="imageOne" />
-                                <img src={`${item.image[0].url}`} alt={item.image[0]} className="imageOne" />
-                                <img src={`${item.image[0].url}`} alt={item.image[0]} className="imageOne" />
-                            </div>
                         </div>
-                    </Link>
+                        <div className="text">
+                            <h3 className="heading">{item.name}</h3>
+                            <p className="paragraph">{item.color}</p>
+                            <p className="paragraph">${item.price}</p>
+                        </div>
+    
+                        <div className="size-box">
+                            {item.sizes.map((size,i)=>{
+                                return (<div className="size" key={i}><small>{size}</small></div>)
+                            })}
+                        </div>
+    
+                        <div className="images-box">
+                            <img src={`${item.image[0].url}`} alt={item.image[0]} className="imageOne" />
+                            <img src={`${item.image[0].url}`} alt={item.image[0]} className="imageOne" />
+                            <img src={`${item.image[0].url}`} alt={item.image[0]} className="imageOne" />
+                            <img src={`${item.image[0].url}`} alt={item.image[0]} className="imageOne" />
+                            <img src={`${item.image[0].url}`} alt={item.image[0]} className="imageOne" />
+                            <img src={`${item.image[0].url}`} alt={item.image[0]} className="imageOne" />
+    
+                            <img src={`${item.image[0].url}`} alt={item.image[0]} className="imageOne" />
+                            <img src={`${item.image[0].url}`} alt={item.image[0]} className="imageOne" />
+                            <img src={`${item.image[0].url}`} alt={item.image[0]} className="imageOne" />
+                            <img src={`${item.image[0].url}`} alt={item.image[0]} className="imageOne" />
+                            <img src={`${item.image[0].url}`} alt={item.image[0]} className="imageOne" />
+                            <img src={`${item.image[0].url}`} alt={item.image[0]} className="imageOne" />
+                        </div>
+                    </div>
                 )
             })
         ):(<h3>No Products</h3>)
