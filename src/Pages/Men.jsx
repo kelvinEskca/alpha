48c900d590ca9dff4f727553241d2da9d75314b6
kelvinEskca <beredugokelvin@gmail.com>
2023-02-13 11:13:36 +0100

    update

--- src/Pages/Men.jsx
+++ src/Pages/Men.jsx
@@ -9,6 +9,7 @@ import MobileNav from "../Components/MobileNav";
 import axios from "axios";
 import Loader from "../Components/Loader";
 import { Link } from "react-router-dom";
+import SlideShow from "../Components/SlideShow";
 const Men = () => {
     const [modal,setModal] = useState(false)
     const [mobile,setMobile] = useState(false)
@@ -113,44 +114,52 @@ const Men = () => {
                         <div className="boxes">
                         {products.length > 0 ? (
                             products.map((item,i)=>{
-                                return (
-                                    <div className="box" key={i}>
-                                        <div className="tag"><small></small></div>
-                                        <div className="image-box">
-                                            <Link to={`/details/${item._id}`}>
-                                                <img src={`${item.image[0].url}`} alt={item.image[0].url} className="imageOne" />
-                                                <img src={`${item.image[1].url}`} alt={item.image[1].url} className="imageTwo" />
-                                            </Link>
-                    
-                                            <div className="quick-add">
-                                                <div className="quick-add-top">
-                                                    <h3 className="heading">Quick Add +</h3>
-                                                </div>
-                    
-                                                <div className="quick-add-bottom">
-                                                    {item.sizes.map((size,i)=>{
-                                                        return (<div className="size" key={i}><small className={`${
-                                                            size === selectedSizes[item._id] ? 'sizeActive' : ''
-                                                        }`} onClick={()=>handleClick(size,item)}>{size}</small></div>)
-                                                    })}
+                                if(item.inStock === true){
+
+                                    const {image} = item;   
+                                    let url = [];                 
+                                    image.map((img)=>{
+                                        return url.push(img.url);
+                                    })
+                                    return (
+                                        <div className="box" key={i}>
+                                            <div className="tag"><small></small></div>
+                                            <div className="image-box">
+                                                <Link to={`details/${item._id}`}>
+                                                    <SlideShow url={url} />
+                                                </Link>
+                        
+                                                <div className="quick-add">
+                                                    <div className="quick-add-top">
+                                                        <h3 className="heading">Quick Add +</h3>
+                                                    </div>
+                        
+                                                    <div className="quick-add-bottom">
+                                                        {item.sizes.map((size,i)=>{
+                                                            return (<div className="size" key={i}><small className={`${
+                                                                size === selectedSizes[item._id] ? 'sizeActive' : ''
+                                                            }`} onClick={()=>handleClick(size,item)}>{size}</small></div>)
+                                                        })}
+                                                    </div>
                                                 </div>
                                             </div>
+                                            <div className="text">
+                                                <h3 className="heading">{item.name}</h3>
+                                                <p className="paragraph">{item.color}</p>
+                                                <p className="paragraph">${item.price}</p>
+                                            </div>
+                        
+                                            <div className="size-box">
+                                                {item.sizes.map((size,i)=>{
+                                                    return (<div className="size" key={i}><small className={`${
+                                                        size === selectedSizes[item._id] ? 'sizeActive' : ''
+                                                    }`} onClick={()=>handleClick(size,item)}>{size}</small></div>)
+                                                })}
+                                            </div>
                                         </div>
-                                        <div className="text">
-                                            <h3 className="heading">{item.name}</h3>
-                                            <p className="paragraph">{item.color}</p>
-                                            <p className="paragraph">${item.price}</p>
-                                        </div>
-                    
-                                        <div className="size-box">
-                                            {item.sizes.map((size,i)=>{
-                                                return (<div className="size" key={i}><small className={`${
-                                                    size === selectedSizes[item._id] ? 'sizeActive' : ''
-                                                }`} onClick={()=>handleClick(size,item)}>{size}</small></div>)
-                                            })}
-                                        </div>
-                                    </div>
-                                )
+                                    )
+                                }
+                                return null
                             })
                         ):(<h3>No Products</h3>)}
                             {products.length > 0 ? (
