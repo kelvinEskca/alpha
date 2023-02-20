114fbcebbbe391e74c3e0b993520b4e0eceeddf7
kelvinEskca <beredugokelvin@gmail.com>
2023-02-20 13:27:59 +0100

    update

--- src/Pages/Women.jsx
+++ src/Pages/Women.jsx
@@ -15,6 +15,7 @@ const Women = () => {
     const [mobile,setMobile] = useState(false)
     const [products,setProducts] = useState([]);
     const [loading,setLoading] = useState(true);
+    const [filteredProdcuts,setFiltereProducts] = useState(null);
     const [selectedSizes, setSelectedSizes] = useState({});
     const {addToCart} = useContext(CartContext);
     const [hero,setHero] = useState([]);
@@ -75,7 +76,19 @@ const Women = () => {
         }
         getHero();
         setLoading(false);
-   },[]);
+
+    },[]);
+    const filterBySize = (size) =>{
+        const filteredItems = products.filter(product => product.sizes.includes(size));
+        setFiltereProducts(filteredItems);
+        console.log(filteredItems);
+    }
+
+    const filterByType = (type) =>{
+        const filteredItems = products.filter(product => product.subcategory.includes(type));
+        setFiltereProducts(filteredItems);
+        console.log(filteredItems);
+    }
 
     const handleClick = (size, item) => {
         setSelectedSizes({ ...selectedSizes, [item._id]: size });
@@ -106,10 +119,9 @@ const Women = () => {
                 <section className="section new">
                     <div className="wrapper">
                         <div className="boxes">
-                        {products.length > 0 ? (
-                            products.map((item,i)=>{
+                        {filteredProdcuts !== null  ? (
+                            filteredProdcuts.map((item,i)=>{
                                 if(item.inStock === true){
-
                                     const {image} = item;   
                                     let url = [];                 
                                     image.map((img)=>{
@@ -155,54 +167,104 @@ const Women = () => {
                                 }
                                 return null
                             })
-                        ):(<h3>No Products</h3>)}
-                            {products.length > 0 ? (
+                        ):(
+                            products.length > 0 ? (
                                 products.map((item,i)=>{
-                                    return (
-                                        <div className="desktop-details inner-deskt-details">
-                                            <div className="desktop-details-top">
-                                                <div className="grid-bottom inner-page-bottom">
-                                                    <span className={openItem === "gender" ? ("high") : ("")}>
-                                                        <div className="top">
-                                                            <Link to='/men'><p className="paragraph">Male</p></Link>
-                                                            <Link to='women'><p className="paragraph">Female</p></Link>
+                                    if(item.inStock === true){
+                                        const {image} = item;   
+                                        let url = [];                 
+                                        image.map((img)=>{
+                                            return url.push(img.url);
+                                        })
+                                        return (
+                                            <div className="box" key={i}>
+                                                <div className="tag"><small></small></div>
+                                                <div className="image-box">
+                                                    <Link to={`details/${item._id}`}>
+                                                        <SlideShow url={url} />
+                                                    </Link>
+                            
+                                                    <div className="quick-add">
+                                                        <div className="quick-add-top">
+                                                            <h3 className="heading">Quick Add +</h3>
                                                         </div>
-
-                                                        <div className="bottom" onClick={()=>openModal("gender")}>
-                                                            <h3 className="heading">Product Gender</h3>
+                            
+                                                        <div className="quick-add-bottom">
+                                                            {item.sizes.map((size,i)=>{
+                                                                return (<div className="size" key={i}><small className={`${
+                                                                    size === selectedSizes[item._id] ? 'sizeActive' : ''
+                                                                }`} onClick={()=>handleClick(size,item)}>{size}</small></div>)
+                                                            })}
                                                         </div>
-                                                    </span>
-
-                                                    <span className={openItem === "size" ? ("high") : ("")}>
-                                                        <div className="top">
-                                                        {item.sizes.map((size)=>{
-                                                            return <p className="paragraph">{size}</p>
-                                                        })}  
-                                                            
-                                                        </div>
-
-                                                        <div className="bottom"onClick={()=>openModal("size")}>
-                                                            <h3 className="heading">Product Size</h3>
-                                                        </div>
-                                                    </span>
-
-                                                    <span className={openItem === "product" ? ("high") : ("")}>
-                                                        <div className="top">
-                                                            <p className="paragraph">{item.subcategory}</p>
-                                                        </div>
-
-                                                        <div className="bottom" onClick={()=>openModal("product")}>
-                                                            <h3 className="heading">Product Type</h3>
-                                                        </div>
-                                                    </span>
+                                                    </div>
+                                                </div>
+                                                <div className="text">
+                                                    <h3 className="heading">{item.name}</h3>
+                                                    <p className="paragraph">{item.color}</p>
+                                                    <p className="paragraph">${item.price}</p>
+                                                </div>
+                            
+                                                <div className="size-box">
+                                                    {item.sizes.map((size,i)=>{
+                                                        return (<div className="size" key={i}><small className={`${
+                                                            size === selectedSizes[item._id] ? 'sizeActive' : ''
+                                                        }`} onClick={()=>handleClick(size,item)}>{size}</small></div>)
+                                                    })}
                                                 </div>
                                             </div>
-                                        </div>
-                                    )
+                                        )
+                                    }
+                                    return null
                                 })
-                            ) : (
-                                ""
-                            )}
+                            ):(<h3>No Products</h3>)
+                        )}
+                        {products.length > 0 ? (
+                            products.map((item,i)=>{
+                                return (
+                                    <div className="desktop-details inner-deskt-details">
+                                        <div className="desktop-details-top">
+                                            <div className="grid-bottom inner-page-bottom">
+                                                <span className={openItem === "gender" ? ("high") : ("")}>
+                                                    <div className="top">
+                                                        <Link to='/men'><p className="paragraph">Male</p></Link>
+                                                        <Link to='women'><p className="paragraph">Female</p></Link>
+                                                    </div>
+
+                                                    <div className="bottom" onClick={()=>openModal("gender")}>
+                                                        <h3 className="heading">Product Gender</h3>
+                                                    </div>
+                                                </span>
+
+                                                <span className={openItem === "size" ? ("high") : ("")}>
+                                                    <div className="top">
+                                                    {item.sizes.map((size)=>{
+                                                        return <p className="paragraph" onClick={() => filterBySize(size)}>{size}</p>
+                                                    })}  
+                                                        
+                                                    </div>
+
+                                                    <div className="bottom"onClick={()=>openModal("size")}>
+                                                        <h3 className="heading">Product Size</h3>
+                                                    </div>
+                                                </span>
+
+                                                <span className={openItem === "product" ? ("high") : ("")}>
+                                                    <div className="top">
+                                                        <p className="paragraph" onClick={() => filterByType(item.subcategory)}>{item.subcategory}</p>
+                                                    </div>
+
+                                                    <div className="bottom" onClick={()=>openModal("product")}>
+                                                        <h3 className="heading">Product Type</h3>
+                                                    </div>
+                                                </span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                )
+                            })
+                        ) : (
+                            ""
+                        )}
                         </div>
                     </div>
                 </section>
